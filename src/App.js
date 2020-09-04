e00f9ccfe520a208e37d50c87820f4aa9ffa9fe1
kharioki <antolisah@gmail.com>
2020-09-04 19:09:08 +0300

    added map styles

--- src/App.js
+++ src/App.js
@@ -19,7 +19,23 @@ import { formatRelative } from 'date-fns';
 // } from '@reach/combobox';
 import '@reach/combobox/styles.css';
 
+import mapStyles from './mapStyles';
+
 const libraries = ['places'];
+const mapContainerStyle = {
+  width: '100vw',
+  height: '100vh'
+};
+const center = {
+  lat: -1.292066,
+  lng: 36.821945
+};
+const options = {
+  styles: mapStyles,
+  disableDefaultUI: true,
+  zoomControl: true
+};
+
 function App() {
   const { isLoaded, loadError } = useLoadScript({
     googleMapsApiKey: process.env.REACT_APP_GOOGLE_MAPS_API_KEY,
@@ -28,7 +44,16 @@ function App() {
 
   if (loadError) return 'Error loading maps';
   if (!isLoaded) return 'Loading maps...';
-  return <div>map</div>;
+  return (
+    <div>
+      <GoogleMap
+        mapContainerStyle={mapContainerStyle}
+        zoom={8}
+        center={center}
+        options={options}
+      ></GoogleMap>
+    </div>
+  );
 }
 
 export default App;
